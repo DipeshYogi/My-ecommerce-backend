fb10f33e420f8d61b3adeb06cb5dcc9c0a64c9f1
milanmangar <milanthapa898@gmail.com>
2020-12-10 06:14:48 +0530

    updated_settings

--- BonoApe/settings.py
+++ BonoApe/settings.py
@@ -25,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '95yc#fyba(67nat1m+c_7=u*!8_1ajj^!zq5ahb(o1^=6@72_%'
+# SECRET_KEY = '95yc#fyba(67nat1m+c_7=u*!8_1ajj^!zq5ahb(o1^=6@72_%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bonoapebackend.herokuapp.com']
 
 
 # Application definition
