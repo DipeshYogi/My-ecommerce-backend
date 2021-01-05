11520154d0c2179c2d2065251dca80ccf4e2b1db
DipeshY <dipeshyogi94@gmail.com>
2021-01-05 18:12:05 +0530

    Added TDD for customer app

--- customerapp/urls.py
+++ customerapp/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('address/<int:id>/', views.GetUserAddress.as_view()),
-    path('address/add/', views.AddUserAddress.as_view()),
-    path('address/update/<int:id>/', views.EditUserAddress.as_view()),
+    path('address/<int:id>/', views.GetUserAddress.as_view(), name ='get_address'),
+    path('address/add/', views.AddUserAddress.as_view(), name ='add_address'),
+    path('address/update/<int:id>/', views.EditUserAddress.as_view(), name ='update_address'),
     path('address/update/active/<int:addrId>/', views.UpdateActiveAddress.as_view()),
-    path('address/delete/<int:addrId>/', views.DeleteUserAddress.as_view())
+    path('address/delete/<int:addrId>/', views.DeleteUserAddress.as_view(), name='delete_address')
 ]
